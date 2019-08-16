b009df68da158703c567eac327cd0858d2322e09
ivan <vansmuhammad@gmail.com>
2019-08-16 19:59:40 +0700

    add: implementing web worker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { Container, Row, Box } from "./style";
 
 function App() {
+  const [message, setMessage] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const myWorker = new Worker("ivan-worker.js");
+    const timeOut = setTimeout(() => {
+      //------send url to worker
+      myWorker.postMessage("https://jsonplaceholder.typicode.com/users");
+      //------receive data from worker
+      myWorker.onmessage = res => {
+        console.log(res, "res");
+        setLoading(false);
+        setMessage(res.data);
+      };
+      //------handling error from worker
+      myWorker.onerror = err => {
+        console.log(err.message, "err");
+        setLoading(false);
+      };
+    }, 500);
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        {message &&
+          message.map(val => (
+            <Box key={val.id}>
+              <p>{val.name}</p>
+              <p>{val.username}</p>
+              <p>{val.email}</p>
+            </Box>
+          ))}
+      </Row>
+    </Container>
   );
 }
 
